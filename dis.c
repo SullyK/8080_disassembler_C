4b2a7e397697a649aa6d78cfac49681c2ebcf34f
Sully <praisethesun.2017@gmail.com>
2024-02-05 18:57:35 +0000

    added header to see if it fixes alpine build issue

--- dis.c
+++ dis.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char *readUserInput(void) {
   printf("Enter your filename, (it has to be in this directory)\n");
