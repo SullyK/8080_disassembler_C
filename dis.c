337b64379212711f05cb0f4b52d9cc145536c081
Sully <praisethesun.2017@gmail.com>
2024-02-08 23:04:23 +0000

    Bufferlookahead + method outline, implement next

--- dis.c
+++ dis.c
@@ -24,21 +24,50 @@ char *readUserInput(void) {
   return NULL;
 }
 
-dis(char* store_output,unsigned char *chunk){
-
+dis(char *store_output, char *lookaheadbuffer, unsigned char *chunk, unsigned int chunk_size) {
+  
+    // on first load, check the look_ahead_buffer to make sure it's empty
+    // if it's empty move on as it means we had no incomplete instructions
+    is_buffer_empty = 1;
     
+    for (int x = 0; x<sizeof(lookaheadbuffer);x++){
+	if(lookaheadbuffer[x] != 0){
+	    is_buffer_empty = 0;
+	    break;
+	}
+    }
+
+    //deal with the buffer here... I need to figure out if I want to 
+    //check each indiviual instruction within the buffer as it might or
+    //do I want to do that before this iteration? for now I will do this
+    //
+    //process byte 1 -> store 1,2,3 bytes in buffer -> process buffer -> clear buffer -> process byte 2 -> store 2,3,4 -> use 2 and 3 (2 byte instruction) -> clear buffer -> process byte 4 etc
+    int pc = 0;
+
+  while (pc < chunk_size) {
+    switch (chunk[prg_cntr]) {
+    case 0x00:
+      printf("NOP");
+      pc += 1;
+    case 0x01:
+      printf("");
+
+    }
+  }
 }
 
 int disAndWrite(unsigned char *chunk, int size) {
 
- //for (int i = 0; i < CHUNK_SIZE; i++) {
-    //printf("%02x\n", chunk[i]);
+  // for (int i = 0; i < CHUNK_SIZE; i++) {
+  // printf("%02x\n", chunk[i]);
   //}
   // testing purposes to check the output
-    char buffer[256];
-    FILE *fp = fopen("dis.txt", "a");
-    for(int i = 0; i < CHUNK_SIZE)
-  return 1;
+  char lookaheadbuffer[3]; // 3 bytes (don't need null term)
+  char buffer[25]; // the buffer shouldn't contain more than 20 char
+                   // to write to output (this is the opcode, not the bytes)
+  FILE *fp = fopen("dis.txt", "a");
+  for (int i = 0; i < CHUNK_SIZE)
+    return 1;
 }
 
 void processFileChunks(FILE *fp, unsigned char *buffer) {
