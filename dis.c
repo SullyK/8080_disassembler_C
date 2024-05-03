95ea18a814afb535133e9a0e4ff12a9140a1bfa0
Sully <praisethesun.2017@gmail.com>
2024-05-03 19:41:47 +0100

    dealing with lookahead

--- dis.c
+++ dis.c
@@ -25,15 +25,15 @@ char *readUserInput(void) {
   return NULL;
 }
 
-// processOpcode() { //unfinished
-//   switch (chunk[prg_cntr]) {
-//   case 0x00:
-//     printf("NOP");
-//     pc += 1;
-//   case 0x01:
-//     printf("");
-//   }
-// }
+processOpcode() { // unfinished
+  switch () {
+  case 0x00:
+    printf("NOP");
+    pc += 1;
+  case 0x01:
+    printf("");
+  }
+}
 
 int instructionSize(uint8_t opcode) { // unfinished
   switch (opcode) {
@@ -87,47 +87,6 @@ int instructionSize(uint8_t opcode) { // unfinished
     return 1;
   } // todo: check these are correct
 }
-// void dis(char *store_output, char *lookaheadbuffer, unsigned char *chunk,
-//     unsigned int chunk_size) {
-//
-//   // on first load, check the look_ahead_buffer to make sure it's empty
-//   // if it's empty move on as it means we had no incomplete instructions
-//   is_buffer_empty = 1;
-//
-//   for (int x = 0; x < sizeof(lookaheadbuffer); x++) {
-//     if (lookaheadbuffer[x] != 0) {
-//       is_buffer_empty = 0;
-//       break;
-//     }
-//   }
-//
-//   // deal with the buffer here... I need to figure out if I want to
-//   // check each indiviual instruction within the buffer as it might or
-//   // do I want to do that before this iteration? for now I will do this
-//   //
-//   // process byte 1 -> store 1,2,3 bytes in buffer -> process buffer -> clear
-//   // buffer -> process byte 2 -> store 2,3,4 -> use 2 and 3 (2 byte
-//   instruction)
-//   // -> clear buffer -> process byte 4 etc
-//
-//   int pc = 0;
-//   // when the buffer isn't empty
-//   if (is_buffer_empty != 1) {
-//     // need to check the first opcode
-//     lookaheadbuffer[0] = processOpcode(
-//   }
-//   while (pc < chunk_size) {
-//     unsigned char opcode = chunk[pc];
-//     processopcode(store_output)//this should take the pointer of small buff
-//   }
-// }
-
-void deal_with_look_ahead(char *lookahead_buffer, uint8_t size) { 0 }
-
-// check lookahead buffer only on iterations that aren't the first
-// check last 2 instructions, if final - 2 required 3 or 2?
-// can use the size of the buffer with a var to see how many instructions
-// are required to processed in the next lookahead
 
 // testing purposes to check the output
 // for (int i = 0; i < bytesread; i++) {
@@ -136,27 +95,43 @@ void deal_with_look_ahead(char *lookahead_buffer, uint8_t size) { 0 }
 //}
 //}
 
-int disAndWrite(unsigned char *chunk, size_t chunk_size, char *lookahead_buffer, 
-                uint8_t *lookahead_size) {
+int disAndWrite(unsigned char *chunk, size_t chunk_size, char *lookahead_buffer, uint8_t *lookahead_req_bytes) {
   FILE *fp = fopen("dis.txt", "a");
-  for (int i = 0; i < chunksize; ++i) {
-    if (lookahead_size != 0) {
+  char opcode_arr[3];
+
+  for (int i = 0; i < chunk_size; ++i) {
+    if (*lookahead_req_bytes != 0) { // deref to get value
       // do_look_ahead_stuff()
+      // I want the compiler to optimise this, i'll need to check it myself, to
+      // see it's doing that
+      //
+      //base-case below - the first value could be the instruction
+      if (*lookahead_req_bytes == 1){
+	  opcode_arr[0] = lookahead_buffer[0];
+	  opcode_arr[1] = lookahead_buffer[1];
+	  opcode_arr[2] = chunk[i];
+      continue; //this could be wrong. should go to next for loop interation
+      }
+
+      //TODO: MAJOR ISSUE WHAT IF IT'S ONLY 1 BYTE IN ARRAY (LAST VALUE + ONLY NEEDS 1 MORE?) NEED
+      //TO DEAL WITH THIS CASE
+      else if(*lookahead_req_bytes == 2){
+ 	  opcode_arr[0] = lookahead_buffer[0];
+	  opcode_arr[1] = 
+	  opcode_arr[2] = 
+      }
+
     }
 
     // lookahead-case - 2nd last + instruction size of 3
     //[4a][9d][] -- example
-    //requires the last instruction from next buffer
+    // requires the last instruction from next buffer
     if (i == (chunk_size - 2) && instructionSize(chunk[i]) == 3) {
-      // fill buffer with the current + last instruction, then set lookahead_size
-      // and return
       lookahead_buffer[0] = chunk[i];
       lookahead_buffer[1] = chunk[i + 1];
       *lookahead_req_bytes = 2;
       return;
     }
-
-    // edgecase - last + instruction size of 2 or 3
 
     if (i == (chunk_size - 1)) {
       uint8_t i_size = instructionSize(chunk[i]);
@@ -174,16 +149,16 @@ int disAndWrite(unsigned char *chunk, size_t chunk_size, char *lookahead_buffer,
 // process the buffer (this should be the chunk of
 // data we read)
 // the bytes read here, will mean if the chunk_size is
-// smaller then its fine 
+// smaller then its fine
 void processFileChunks(FILE *fp, unsigned char *buffer) {
 
   char lookahead_buffer[3];
   uint8_t lookahead_req_bytes = 0;
   size_t bytesread;
 
-  //bytes read into buffer, loops for the whole file
-  while ((bytesread = fread(buffer, 1, CHUNK_SIZE, fp)) >0) { 
-    disAndWrite(buffer, bytesread, lookahead_buffer,&lookahead_req_bytes); 
+  // bytes read into buffer, loops for the whole file
+  while ((bytesread = fread(buffer, 1, CHUNK_SIZE, fp)) > 0) {
+    disAndWrite(buffer, bytesread, lookahead_buffer, &lookahead_req_bytes);
   }
 
   if (ferror(fp)) {
