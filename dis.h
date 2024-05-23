69a109a07347687f52e5b3d4cb3f78ca06379a66
Sully <praisethesun.2017@gmail.com>
2024-05-23 19:27:05 +0100

    refactored a bit, this commit before I mess with disandwrite func

---
+++ dis.h
@@ -0,0 +1,22 @@
+#ifndef DIS_H
+#define DIS_H
+
+#define _GNU_SOURCE 1
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define CHUNK_SIZE 1024 // 16KiB = 16 * 1024 (reduced to 1KiB for testing)
+
+char *read_user_input(void);
+void process_opcode(unsigned char *buffer);
+int instructionSize(uint8_t opcode);
+void disAndWrite(unsigned char *chunk, size_t chunk_size,
+                 unsigned char *lookahead_buffer, uint8_t *lookahead_req_bytes,
+                 bool *ignore_third_byte);
+void processFileChunks(FILE * fp, unsigned char *buffer);
+
+#endif
